5585b87e3ec6990f62a96db5a6c2f0851993c935
sergey.perov <sergey.perov@soft-machine.ru>
2022-05-06 00:54:51 +0300

    update

--- 6.classes/students_and_mentor.py
+++ 6.classes/students_and_mentor.py
@@ -8,7 +8,7 @@ class Student:
         self.grades = {}
 
     def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -23,7 +23,8 @@ class Student:
                 calc = calc + grade
                 x = len(course)
         sum = round(calc/x, 1)
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {sum}'
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания:{sum}\
+              \nКурсы обучения: {",".join(self.courses_in_progress)}\nЗавершенные курсы: {",".join(self.finished_courses)}'
         return res
 
 class Mentor:
@@ -31,12 +32,19 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.grades = {}
 
 class Lecturer(Mentor):
-    grades = {}
+    # grades = {}
     def __str__(self):
+        calc=int()
+        for course in self.grades.values():
+            for grade in course:
+                calc = calc + grade
+                x = len(course)
+        sum = round(calc/x, 1)
 
-        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{sum}'
         return res
 
 
@@ -54,16 +62,25 @@ class Reviewer(Mentor):
         return res
 
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+some_student = Student('Ruoy', 'Eman', 'your_gender')
+some_student.courses_in_progress += ['Python']
+some_student.courses_in_progress += ['Git']
+some_student.finished_courses += ['Введение в программирование']
  
-cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+some_reviewer = Reviewer('Some', 'Buddy')
+some_reviewer.courses_attached += ['Python']
+some_lecturer = Lecturer('Some', 'Buddy')
+some_lecturer.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 9)
-cool_mentor.rate_hw(best_student, 'Python', 10)        
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 9)
+some_reviewer.rate_hw(some_student, 'Python', 10)       
+some_student.rate_hw(some_lecturer, 'Python', 10)
+some_student.rate_hw(some_lecturer, 'Python', 9)
+some_student.rate_hw(some_lecturer, 'Python', 10)   
 
  
-print(best_student.grades)
-print(best_student)
+# print(some_student.grades)
+print(some_reviewer)
+print(some_lecturer)
+print(some_student)
