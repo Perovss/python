37fb5095f1c89cafa23afbf888731d0749b4fb88
sergey.perov <sergey.perov@soft-machine.ru>
2022-05-07 00:00:06 +0300

    task3

--- 6.classes/students_and_mentor.py
+++ 6.classes/students_and_mentor.py
@@ -16,14 +16,16 @@ class Student:
         else:
             return 'Ошибка'
 
-    def __str__(self):
+    def sum(self):
         calc=int()
         for course in self.grades.values():
             for grade in course:
-                calc = calc + grade
-                x = len(course)
-        sum = round(calc/x, 1)
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания:{sum}\
+                calc += grade
+        sum = round(calc/len(course), 1)
+        return sum
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания:{self.sum()}\
               \nКурсы обучения: {",".join(self.courses_in_progress)}\nЗавершенные курсы: {",".join(self.finished_courses)}'
         return res
 
@@ -32,21 +34,24 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        self.grades = {}
+
 
 class Lecturer(Mentor):
-    # grades = {}
-    def __str__(self):
+    grades = {}
+    
+    def sum(self):
         calc=int()
         for course in self.grades.values():
             for grade in course:
-                calc = calc + grade
-                x = len(course)
-        sum = round(calc/x, 1)
+                calc += grade
+        sum = round(calc/len(course), 1)
+        return sum
 
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{sum}'
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.sum()}'
         return res
-
+    def __lt__(self,other):
+        return self.sum() > other.sum()
 
 class Reviewer(Mentor):    
     def rate_hw(self, student, course, grade):
@@ -57,10 +62,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+    
     def __str__(self):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
-
  
 some_student = Student('Ruoy', 'Eman', 'your_gender')
 some_student.courses_in_progress += ['Python']
@@ -72,15 +77,17 @@ some_reviewer.courses_attached += ['Python']
 some_lecturer = Lecturer('Some', 'Buddy')
 some_lecturer.courses_attached += ['Python']
  
-some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 1)
 some_reviewer.rate_hw(some_student, 'Python', 9)
-some_reviewer.rate_hw(some_student, 'Python', 10)       
-some_student.rate_hw(some_lecturer, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 10)  
+     
+some_student.rate_hw(some_lecturer, 'Python', 8)
 some_student.rate_hw(some_lecturer, 'Python', 9)
 some_student.rate_hw(some_lecturer, 'Python', 10)   
-
- 
-# print(some_student.grades)
+# Задание № 3
+# 1. Перегрузите магический метод __str__ у всех классов
 print(some_reviewer)
 print(some_lecturer)
 print(some_student)
+# 2. Реализуйте возможность сравнивать
+print(some_lecturer < some_student)
