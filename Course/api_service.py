2b9c7aea7c1155ad5ab47f6139c4f0384d8d881a
sergey.perov <sergey.perov@soft-machine.ru>
2022-06-14 08:50:11 +0300

    update

---
+++ Course/api_service.py
@@ -0,0 +1,72 @@
+import time, requests, json, os
+from pprint import pprint
+dict_photo = {}
+
+def write_json(data):
+    with open('photos.json', 'w') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+def get_lagest(size_dict):
+    weight = {'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 'm' : 10, 'x' : 20, 'y' : 30, 'z' : 40, 'w' : 50}
+    size_dict['type'] = weight[size_dict['type']]
+    return size_dict['type']
+    # if size_dict['width'] >= size_dict['height']:
+    #     return size_dict['width']
+    # else:
+    #     return size_dict['height']      
+
+
+class VKservice:
+    def __init__(self, url, token):
+        self.url = url
+        self.token = token
+
+    def request_get(self):
+        url_photos = self.url + 'photos.get'
+        params = {
+            'owner_id': '4321910',
+            'access_token': self.token,
+            'v': '5.131',
+            'album_id': 'profile',
+            'count': 100,
+            'extended': '1'
+        }
+        res = requests.get(url_photos, params=params).json()
+        photos = res['response']['items']
+        for photo in photos:
+            # write_json(photo['sizes'])
+            sizes = (photo['sizes'])
+            link_photo = max(sizes, key = get_lagest)['url']
+            likes = (photo['likes'])['count']
+            print(link_photo)
+
+            # for qwert in sizes:
+            #     if qwert['type'] == 'z':
+            #         qwerty = {qwert['url']: likes['count']}
+            #         dict_photo.update(qwerty)
+
+            download_photo(link_photo, 'Files', likes)
+
+def download_photo(url, disk_file_path, name):
+    """
+    Функция скачивания файла по интернет ссылке url в папку disk_file_path
+    @param url: url скачиваемого файла
+    @param disk_file_path: локальная папка, куда будет скачан файл
+    @return: полное имя файла, результат скачивания
+    """
+    # Скачивание файла
+    res = requests.get(url, stream=True, allow_redirects=True)
+ 
+    # Вычленение навзания файла
+    # url = res.url.split('/')[-1].split('?')[0]
+    url = str(name) +'.jpg'
+
+    # Вычисление полного пути
+    filepath = os.path.join(disk_file_path, url)
+
+    # Сохраение файла в локальную папку disk_file_path
+    with open(filepath, 'wb') as image:
+        if res.ok:
+            for content in res.iter_content(1024):
+                if content:
+                    image.write(content)
--- Course/main.py
+++ Course/main.py
@@ -1,44 +1,15 @@
-import time
-import requests
+import time, api_service
 from pprint import pprint
+import os
 with open('token.txt' , "r") as token_in_file:
     token = token_in_file.read().strip()
+path="files"
+url = "https://api.vk.com/method/"
 
-def request_get():
-    URL = 'https://api.vk.com/method/photos.get'
-    params = {
-        'owner_id': '4321910',
-        'access_token': token,
-        'v': '5.131',
-        'album_id': 'profile',
-        'count': 300,
-        'extended': '1'
-        # 'fields': 'education,sex'
-    }
-    res = requests.get(URL, params=params).json()
-    photos = res['response']['items']
-    for photo in photos:
-        file_dir = (photo['sizes'])
-        likes = (photo['likes'])
-        for qwert in file_dir:
-            if qwert['type'] == 'z':
-                print(qwert['url'])
-                print(likes['count'])
+os.system('cls||clear')
 
+hello = api_service.VKservice(url, token)
+hello.request_get()
+# api_service.download_photo(hello.request_get(), path)
 
-request_get()
-# def group_search(q, sorting=0):
-#     params = {
-#         'q': q,
-#         'access_token': token,
-#         'v': '5.131',
-#         'sort': sorting,
-#         'count': 300
-#     }
-#     req = requests.get("https://api.vk.com/method/groups.search", params).json()
-#     req = req['response']['items']
-#     return req
-# target_group = group_search("python")
-# # pprint(target_group)
-# # target_group_ids = ','.join([str(group['id']) for group in target_group])
-# # pprint(target_group_ids)
+# api_service.download_photo(url, path)
