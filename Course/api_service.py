a059d7f921125bed30e51d63fb4a4fefc146547a
sergey.perov <sergey.perov@soft-machine.ru>
2022-06-16 12:11:27 +0300

    finish

--- Course/api_service.py
+++ Course/api_service.py
@@ -2,7 +2,7 @@ import requests, json, os, time
 from pprint import pprint
 from datetime import datetime
 from progress.bar import IncrementalBar
-dict_photo = {}
+dict_photo = []
 photo_list = []
 file_list = []
 
@@ -10,24 +10,27 @@ def write_json(data):
     with open('photos.json', 'w') as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
 
-def get_lagest(size_dict):
-    weight = {'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 'm' : 10, 'x' : 20, 'y' : 30, 'z' : 40, 'w' : 50}
-    size_dict['type'] = weight[size_dict['type']]
-    return size_dict['type']
+# def get_lagest(size_dict):
+#     weight = {'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 'm' : 10, 'x' : 20, 'y' : 30, 'z' : 40, 'w' : 50}
+#     size_dict['type'] = weight[size_dict['type']]
+#     return size_dict['type']
 
 class VKservice:
-    def __init__(self, url, token):
+    def __init__(self, url, path, id, token):
         self.url = url
+        self.path = path
         self.token = token
+        self.id = id
 
-    def request_get(self):
+    def request_get(self, count, offset = 0):
         url_photos = self.url + 'photos.get'
         params = {
-            'owner_id': '4321910',
+            'owner_id': self.id,
             'access_token': self.token,
             'v': '5.131',
+            'offset': offset,
             'album_id': 'profile',
-            'count': 100,
+            'count': count,
             'extended': '1'
         }
         res = requests.get(url_photos, params=params).json()
@@ -36,62 +39,54 @@ class VKservice:
         for photo in photos:
             bar.next()
             time.sleep(1)
-            write_json(photo['sizes'])
-            sizes = (photo['sizes'])
-            link_photo = max(sizes, key = get_lagest)['url']
+            sizes = (photo['sizes'][-1])
+            link_photo = sizes['url']
             likes = (photo['likes'])['count']
             photo_list.append(likes)
             if likes in photo_list and photo_list.count(likes) >1:               
                 likes = str(likes) +'_' +str(datetime.now().strftime("%Y-%m-%d %H-%M"))
-            download_photo(link_photo, os.getcwd() + '/Files', likes)
+            photo_list.append(likes)
+            download_photo(link_photo, os.getcwd() + '/'+ self.path, likes)
+            extension = link_photo.split('.')[-1].split('?')[0]
+            sizes["files"] = f'{likes}.{extension}'
+            dict_photo.append(sizes)
         bar.finish()
+        write_json(dict_photo)
 
 def download_photo(url, disk_file_path, name):
-    """
-    Функция скачивания файла по интернет ссылке url в папку disk_file_path
-    @param url: url скачиваемого файла
-    @param disk_file_path: локальная папка, куда будет скачан файл
-    @return: полное имя файла, результат скачивания
-    """
-    # Скачивание файла
     res = requests.get(url, stream=True, allow_redirects=True)
- 
-    # Вычленение навзания файла
     extension = res.url.split('.')[-1].split('?')[0]
     url = str(name) +'.'+str(extension)
     file_list.append(url)
-    # Вычисление полного пути
     filepath = os.path.join(disk_file_path, url)
 
-    # Сохраение файла в локальную папку disk_file_path
-    with open(filepath, 'wb') as image:
-        if res.ok:
-            for content in res.iter_content(1024):
-                if content:
-                    image.write(content)
 
-class YaUploader:
-    def __init__(self, token: str):
+    with open(filepath, 'wb') as image:
+        for content in res.iter_content(1024):
+            if content:
+                image.write(content)
+
+class YaUploader():
+    def __init__(self, path, token):
         self.token = token
+        self.path = path
+
 
     def _get_upload_link(self, disk_file_path):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {self.token}'}
         params = {"path": disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
-
         return response.json()
 
     def upload_file_to_disk(self,path, filename = file_list):
-        bar = IncrementalBar('Загрузка фото из Yandex.Disc', max = len(file_list), )
+        bar = IncrementalBar('Загрузка фото в Yandex.Disc', max = len(file_list), )
         for filename in file_list:
             bar.next()
             time.sleep(1)
             disk_file_path = (f'{path}/{filename}')
             href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
-            response = requests.put(href, data=open(f"{os.getcwd()}/Files/{filename}", 'rb'), timeout=10)
+            response = requests.put(href, data=open(f"{os.getcwd()}/{self.path}/{filename}", 'rb'), timeout=10)
         bar.finish()
-            # response.raise_for_status()
-            # if response.status_code == 201:
-            #     print(f"файл - {disk_file_path} загрузка выполнена.")
+
         
--- Course/main.py
+++ Course/main.py
@@ -10,14 +10,37 @@ def open_token(filename:str):
 path = 'files'
 ya_path = 'course'
 url = 'https://api.vk.com/method/'
+basic_count = 10
+basic_id = 4321910
 
 os.system('cls||clear')
 
-vk_import = api_service.VKservice(url, open_token('token.txt'))
-vk_import.request_get()
+while True:
+    id = input(f'Введите ID пользователя в VK, по умолчанию ID = {basic_id}: ')
+    if id == '':
+        id = str(basic_id)
+    else:
+        try:
+            value = int(id)
+        except ValueError:
+           print('ID - это численное значение')
+           continue
+    count = input(f'Введите количество скачиваемых фото по умолчанию - {basic_count}: ')
+    if count == '':
+        count = str(basic_count)
+    else:
+        try:
+            value = int(count)
+        except ValueError:
+           print('ID - это численное значение')
+           continue
 
-YA_export = api_service.YaUploader(open_token('token_yandex.txt'))
+    vk_import = api_service.VKservice(url, path, id, open_token('token.txt'))
+    vk_import.request_get(count)
 
-YA_export.upload_file_to_disk(ya_path)
+    YA_export = api_service.YaUploader(path, open_token('token_yandex.txt'))
+    YA_export.upload_file_to_disk(ya_path)
 
-# api_service.write_json(photo['sizes'])
+    print ('Выходные данные находяться в photos.json')
+    print('Ссылка на Yandex.Disx https://disk.yandex.ru/d/dR9b2W5qhYg7iw')
+    break
