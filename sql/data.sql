48be64c8f7577fc02d1d830c7baf9cd4d7d091c9
sergey.perov <sergey.perov@soft-machine.ru>
2022-06-23 00:05:24 +0300

    update

--- sql/data.sql
+++ sql/data.sql
@@ -14,7 +14,7 @@ INSERT INTO public.albums (name_album,release_year) VALUES
             ('The Best of the Early Years', 2007),
             ('Я Открою Свое Сердце', 2012),
             ('Чёрный пёс Петербург', 1993),
-            ('Мой альбом', 2021),
+            ('Баста 40', 2020),
             ('Самолет', 2018),
             ('С самых низов', 2016)
 
@@ -76,32 +76,123 @@ INSERT INTO public.singles (id_album,name_single,duration) VALUES
             (3,'Someday',221),
             (3,'Hey, Look Me Over',158),
             (3,'I Still Get Jealous',133),
-            (3,'Moon River',179)
-            
+            (3,'Moon River',179),
+            (3,'Please Love Me',234),
+            (3,'Woke Up This Morning',433),
+            (3,'You Upset Me Baby',345),
+            (3,'Dont Look Now, But Ive Got The Blues',341)
 
+INSERT INTO public.singles (id_album,name_single,duration) VALUES
+            (4,'Intro (Live)',270),
+            (4,'Я вижу пустоту (Live)',440),
+            (4,'Я к вечности шагаю (Live)',500),
+            (4,'Жене (Live)',225),
+            (4,'За воротами времени (Live)',226),
+            (4,'Живу и таю (Live)',400),
+            (4,'Почти устал (Live)',414)
 
-1		
-2		
-3		
-4		3 O'Clock Blues
-5		Please Love Me
-6		Woke Up This Morning
-7		You Upset Me Baby
-8		Every Day I Have The Blues
-9		When My Heart Beats Like A Hammer
-10		Ten Long Years
-11		Sweet Little Angel
-12		Don't Look Now, But I've Got The Blues
-13		Early In The Morning
-14		Days Of Old
-15		Mean Old Frisco
-16		Catfish Blues Aka Fishin' After Me
-17		Sweet Sixteen Pts 1&2
-18		I'll Survive
-19		Downhearted Aka How Blue Can You Get?
-20		Bad Case Of Love
-21		Rock Me Baby
-22		Blues Stay Away From Me
-23		Five Long Years
-24		That Evil Child
-25		Why I Sing The Blues
+INSERT INTO public.singles (id_album,name_single,duration) VALUES
+            (5,'Пролог',225),
+            (5,'Новые блокадники',328),
+            (5,'Styx',269),
+            (5,'Храм',257),
+            (5,'Беда',510),
+            (5,'Ты не один',308),
+            (5,'Я остановил время',308),
+            (5,'В это',286),
+            (5,'Post интеллигент (бонус CD-издания)',287),
+            (5,'Террорист (бонус CD-издания)',504)
+    
+INSERT INTO public.singles (id_album,name_single,duration) VALUES
+            (6,'Интро',145),
+            (6,'С самых низов',339),
+            (6,'Верил всегда',344),
+            (6,'Время догонит нас',306),
+            (6,'+100500',348),
+            (6,'Дочь огня',249),
+            (6,'Я вижу',370),
+            (6,'Не дотянуться до звёзд',249),
+            (6,'ГотеMMосква',271),
+            (6,'Молодость',350)
+
+INSERT INTO public.singles (id_album,name_single,duration) VALUES
+            (7,'Амстердам супер расколбас',182),
+            (7,'Тревога!',285),
+            (7,'Самолёт',527),
+            (7,'мой cамолет (Всю ночь под Феном)',352),
+            (7,'Сначала по одной потом по половинке всё что нужно для хорошей вечеринки В',313),
+            (7,'Всё,что нужно для хорошей вечеринки',248),
+            (7,'Половинка (Polovinka Re-Work) (Radio Cut)',339),
+            (7,'North-West GSM original mix',342)
+
+INSERT INTO public.singles (id_album,name_single,duration) VALUES
+            (8,'С самых низов',213),
+            (8,'Эльдорадо',247),
+            (8,'Рутина',315)
+
+INSERT INTO public.collections (name_collection,release_year) VALUES
+            ('Cборник 1',2015), 
+            ('Cборник 2',2016), 
+            ('Cборник 3',2017), 
+            ('Cборник 4',2018), 
+            ('Cборник 5',2019), 
+            ('Cборник 6',2020), 
+            ('Cборник 7',2021), 
+            ('Cборник 8',2022)
+
+INSERT INTO public.collections_singles (id_collection,id_single) VALUES
+            (1,1),
+            (1,2),
+            (8,3),
+            (1,4),
+            (1,5),
+            (1,6),
+            (1,7),
+            (8,8),
+            (2,9),
+            (2,10),
+            (5,11),
+            (2,12),
+            (2,13),
+            (6,14),
+            (2,15),
+            (2,16),
+            (7,17),
+            (3,18),
+            (3,19),
+            (3,20),
+            (3,21),
+            (3,22),
+            (4,23),
+            (3,24),
+            (4,25),
+            (3,26),
+            (1,27),
+            (1,28),
+            (3,29),
+            (4,30),
+            (1,31),
+            (4,32),
+            (4,33),
+            (5,34),
+            (4,35),
+            (4,36),
+            (5,37),
+            (5,38),
+            (5,39),
+            (5,40),
+            (5,41),
+            (6,42),
+            (6,43),
+            (6,44),
+            (6,45),
+            (7,46),
+            (7,47),
+            (7,48),
+            (7,49),
+            (7,50),
+            (7,51),
+            (8,52),
+            (8,53),
+            (8,54),
+            (8,55)
