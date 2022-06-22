c4f46d04c575f560a586b980e63a8f60e4ac6ef5
sergey.perov <sergey.perov@soft-machine.ru>
2022-06-22 22:40:00 +0300

    update

--- sql/data.sql
+++ sql/data.sql
@@ -67,5 +67,41 @@ INSERT INTO public.singles (id_album,name_single,duration) VALUES
             (2,'Hey, Look Me Over',158),
             (2,'I Still Get Jealous',133),
             (2,'Moon River',179)
+            
+INSERT INTO public.singles (id_album,name_single,duration) VALUES
+            (3,'BB Boogie',237),
+            (3,'Shes Dynamite',215),
+            (3,'A Lot of Livin to Do',156),
+            (3,'A Kiss to Build a Dream On',271),
+            (3,'Someday',221),
+            (3,'Hey, Look Me Over',158),
+            (3,'I Still Get Jealous',133),
+            (3,'Moon River',179)
+            
 
 
+1		
+2		
+3		
+4		3 O'Clock Blues
+5		Please Love Me
+6		Woke Up This Morning
+7		You Upset Me Baby
+8		Every Day I Have The Blues
+9		When My Heart Beats Like A Hammer
+10		Ten Long Years
+11		Sweet Little Angel
+12		Don't Look Now, But I've Got The Blues
+13		Early In The Morning
+14		Days Of Old
+15		Mean Old Frisco
+16		Catfish Blues Aka Fishin' After Me
+17		Sweet Sixteen Pts 1&2
+18		I'll Survive
+19		Downhearted Aka How Blue Can You Get?
+20		Bad Case Of Love
+21		Rock Me Baby
+22		Blues Stay Away From Me
+23		Five Long Years
+24		That Evil Child
+25		Why I Sing The Blues
