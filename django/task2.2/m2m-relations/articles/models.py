44bdc953b95c66e3521e165633bbdfa7174f99aa
sergey.perov <sergey.perov@soft-machine.ru>
2022-09-24 22:33:48 +0300

    qwerty

--- django/task2.2/m2m-relations/articles/models.py
+++ django/task2.2/m2m-relations/articles/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-
 class Article(models.Model):
-
+    id = models.AutoField(unique=True, primary_key=True)
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
@@ -11,6 +10,16 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+class Tags(models.Model):
+    id = models.AutoField(unique=True, primary_key=True)
+    name = models.CharField(max_length=256, verbose_name='Название Раздел')
+
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
 
     def __str__(self):
         return self.title
--- django/task2.2/m2m-relations/articles/views.py
+++ django/task2.2/m2m-relations/articles/views.py
@@ -5,8 +5,8 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
+    content = Article.objects.all()
+    context = {'object_list': content}
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
