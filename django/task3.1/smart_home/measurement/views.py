04452b7d3f3c53c388c11fd2d91b1e3d2983ba52
sergey.perov <sergey.perov@soft-machine.ru>
2022-10-04 00:14:31 +0300

    ttask3.1

--- django/task3.1/smart_home/measurement/views.py
+++ django/task3.1/smart_home/measurement/views.py
@@ -1,16 +1,17 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
 from measurement.models import Sensor, Measurement
 from measurement.serializers import SensorDetailSerializer, SensorSerializer, MeasurementSerializer
+
 
 class Sensors(ListCreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
+
 class Measurements(ListCreateAPIView):
     queryset = Measurement.objects.all()
     serializer_class = MeasurementSerializer
+
 
 class SingleSensor(RetrieveUpdateAPIView):
     queryset = Sensor.objects.all()
