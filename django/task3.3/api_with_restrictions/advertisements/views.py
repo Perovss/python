b5494cae549776678d90ce1987b03e5fb91901a3
sergey.perov <sergey.perov@soft-machine.ru>
2022-10-26 16:19:03 +0300

    fix

--- django/task3.3/api_with_restrictions/advertisements/views.py
+++ django/task3.3/api_with_restrictions/advertisements/views.py
@@ -2,9 +2,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
+from advertisements.permission import IsOwnerOrReadOnly
 from advertisements.serializers import AdvertisementSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
@@ -12,6 +13,8 @@ class AdvertisementViewSet(ModelViewSet):
     serializer_class = AdvertisementSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class =AdvertisementFilter
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
